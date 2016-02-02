45fa725ef2d2a7807914a2b04c6672d104271141
Ryan Duryea <aguynamedryan@gmail.com>
2016-02-01 17:12:30 -0800

    Add examples and info to webapp

--- app.rb
+++ app.rb
@@ -80,19 +80,49 @@ __END__
 
 @@index
 %form#reverse(action="/" method="GET")
+  %p Examples (click to view results):
+  %dl
+    %dt
+      %a{href: "http://explodacode.jigsawanalytics.com/?code_str=412&vocabulary=ICD-9" } 412
+    %dd Search by three digit code only
+    %dt
+      %a{href: "http://explodacode.jigsawanalytics.com/?code_str=410.x&vocabulary=ICD-9" } 410.x
+    %dd Search for all four-digit codes starting with 410
+    %dt
+      %a{href: "http://explodacode.jigsawanalytics.com/?code_str=410.xx&vocabulary=ICD-9" } 410.xx
+    %dd Search for all five-digit codes starting with 410
+    %dt
+      %a{href: "http://explodacode.jigsawanalytics.com/?code_str=410.y&vocabulary=ICD-9" } 410.y
+    %dd Search for all four-digit or five-digit codes starting with 410
+    %dt
+      %a{href: "http://explodacode.jigsawanalytics.com/?code_str=412%2C+410.x1&vocabulary=ICD-9" } 412, 410.x1
+    %dd Search more than one code at a time: three-digit code 412 and any five-digit code starting with 410 and ending in 1
+    %dt
+      %a{href: "http://explodacode.jigsawanalytics.com/?code_str=410-412&vocabulary=ICD-9" } 410-412
+    %dd Search for all codes between 410 through 412
+  %br
   = "Enter Code String"
   %input#word{ type: "text", name: "code_str", value:@code_str.first }
   %br
-  = "And select a vocabulary"
+  = "Select a vocabulary"
   %select#vocabulary{ type: "text", name: "vocabulary" }
     %option{ label: '', value: '' }
     - @vocabularies.each do |vocab|
-      %option{ label: vocab, value: vocab }
+      %option{ label: vocab, value: vocab, selected: vocab == @vocabulary }
+  %br
+  = "And click "
   %input(type="submit" value="Search")
+  %br
   - if @codes.empty?
     %p= "Nothing matched #{@search}"
   - else
+    %p
+      Codes in comma-delimited list (great for
+      %a{ href: "https://github.com/outcomesinsights/conceptql" } ConceptQL
+      codelists)
     %p= @codes.sort.join(", ")
+    %br
+    %p Codes in a list
     %ul
     - @codes.each_with_index do |code|
       %li= code
